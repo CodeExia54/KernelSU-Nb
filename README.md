5588c5fed9d40b46b4e3e0b3a81e2cac0735d13d
Your Name <you@example.com>
2025-02-25 10:31:05 +0530

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+<div align="center">
+  <h1>Automate building of GKI Kernel and LKMs</h1>
+  <h3><i>Powered By GitHub Actions</i></h3>
+</div>
+
+## Credits
+- [KernelSU](https://github.com/tiann/KernelSU)
---
+++ exianb/exianb.c
@@ -0,0 +1,136 @@
+#include <linux/module.h>
+#include <linux/tty.h>
+#include <linux/miscdevice.h>
+#include "comm.h"
+#include "memory.h"
+#include "process.h"
+
+#include <linux/kernel.h> 
+#include <linux/module.h> 
+#include <linux/proc_fs.h> 
+#include <linux/sched.h> 
+#include <linux/uaccess.h> 
+#include <linux/version.h> 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 10, 0) 
+#include <linux/minmax.h> 
+#endif 
+ 
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 6, 0) 
+#define HAVE_PROC_OPS 
+#endif 
+ 
+#define PROCFS_MAX_SIZE 2048UL 
+#define PROCFS_ENTRY_FILENAME "exianb" 
+ #define DEVICE_NAME "exianb"
+static char *my_string = "exianb";
+static struct proc_dir_entry *our_proc_file; 
+
+// static struct miscdevice dispatch_misc_device;
+module_param(my_string, charp, 0644); // String parameter
+MODULE_PARM_DESC(my_string, "Parameter");
+
+int dispatch_open(struct inode *node, struct file *file) {
+    return 0;
+}
+
+int dispatch_close(struct inode *node, struct file *file) {
+    return 0;
+}
+
+long dispatch_ioctl(struct file* const file, unsigned int const cmd, unsigned long const arg) {
+    static COPY_MEMORY cm;
+    static MODULE_BASE mb;
+    static char name[0x100] = {0};
+
+    switch (cmd) {
+        case OP_READ_MEM:
+            {
+                if (copy_from_user(&cm, (void __user*)arg, sizeof(cm)) != 0) {
+                    pr_err("OP_READ_MEM copy_from_user failed.\n");
+                    return -1;
+                }
+                if (read_process_memory(cm.pid, cm.addr, cm.buffer, cm.size) == false) {
+                    pr_err("OP_READ_MEM read_process_memory failed.\n");
+                    return -1;
+                }
+            }
+            break;
+        case OP_WRITE_MEM:
+            {
+                if (copy_from_user(&cm, (void __user*)arg, sizeof(cm)) != 0) {
+                    return -1;
+                }
+                if (write_process_memory(cm.pid, cm.addr, cm.buffer, cm.size) == false) {
+                    return -1;
+                }
+            }
+            break;
+        case OP_MODULE_BASE:
+            {
+                if (copy_from_user(&mb, (void __user*)arg, sizeof(mb)) != 0 
+                ||  copy_from_user(name, (void __user*)mb.name, sizeof(name)-1) !=0) {
+                    pr_err("OP_MODULE_BASE copy_from_user failed.\n");
+                    return -1;
+                }
+                mb.base = get_module_base(mb.pid, name);
+                if (copy_to_user((void __user*)arg, &mb, sizeof(mb)) !=0) {
+                    pr_err("OP_MODULE_BASE copy_to_user failed.\n");
+                    return -1;
+                }
+            }
+            break;
+        default:
+            break;
+    }
+return 0;
+}
+
+#ifdef HAVE_PROC_OPS 
+static struct proc_ops file_ops_4_our_proc_file = { 
+    .proc_ioctl = dispatch_ioctl,
+    .proc_open = dispatch_open, 
+    .proc_release = dispatch_close, 
+}; 
+#else 
+static const struct file_operations file_ops_4_our_proc_file = { 
+    .unlocked_ioctl = dispatch_ioctl,
+    .open = dispatch_open, 
+    .release = dispatch_close, 
+}; 
+#endif 
+
+int __init driver_entry(void) {
+    // int ret;
+    pr_info("[+] device load");
+
+    our_proc_file = proc_create(my_string/*PROCFS_ENTRY_FILENAME*/, 0644, NULL, 
+                                &file_ops_4_our_proc_file); 
+    if (our_proc_file == NULL) { 
+        pr_debug("Error: Could not initialize /proc/%s\n", 
+                 PROCFS_ENTRY_FILENAME); 
+        return -ENOMEM; 
+    } 
+    proc_set_size(our_proc_file, 80); 
+    proc_set_user(our_proc_file, GLOBAL_ROOT_UID, GLOBAL_ROOT_GID); 
+ 
+    pr_debug("/proc/%s created\n", PROCFS_ENTRY_FILENAME); 
+    return 0; // ret;
+}
+
+void __exit driver_unload(void) {
+    pr_info("[+] device unloaded");
+    proc_remove(our_proc_file); 
+}
+
+module_init(driver_entry);
+module_exit(driver_unload);
+
+MODULE_AUTHOR("exianb");
+MODULE_DESCRIPTION("exianb");
+MODULE_LICENSE("GPL");
+MODULE_VERSION("1.0");
+
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 0, 0)
+MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
+#endif
+
