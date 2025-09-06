921eba7b425dce80521111f5e547d95beea93b46
D-R-99 <75912089+D-R-99@users.noreply.github.com>
2025-09-06 10:34:31 +0530

    Update core.c

--- ovo/core.c
+++ ovo/core.c
@@ -73,3 +73,9 @@ MODULE_AUTHOR("fuqiuluo");
 MODULE_LICENSE("GPL");
 MODULE_DESCRIPTION("https://github.com/ovo");
 MODULE_VERSION("1.0.0");
+
+/*
+MODULE_AUTHOR("exianb");
+MODULE_DESCRIPTION("exianb");
+MODULE_LICENSE("GPL");
+*/
