b6498ec335345ce8f22912950f1d2b09e123c089
CodeExia54 <codeexia97@gmail.com>
2025-09-08 18:26:15 +0530

    Update mmuhack.c

--- ovo/mmuhack.c
+++ ovo/mmuhack.c
@@ -187,8 +187,9 @@ pte_t *page_from_virt_user(struct mm_struct *mm, unsigned long addr) {
         return NULL;
     }
 	if(!pte_present(*pte)) {
-        return NULL;
 		pr_info("[ovo] yaha dala h log\n");
+        return NULL;
+		
 	}
 
     return pte;
