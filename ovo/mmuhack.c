367dcbb9887a31af5f9345dd8645362b8273e5b7
CodeExia54 <codeexia97@gmail.com>
2025-09-10 00:40:48 +0530

    Update mmuhack.c

--- ovo/mmuhack.c
+++ ovo/mmuhack.c
@@ -81,8 +81,7 @@ pte_t *page_from_virt_kernel(unsigned long addr) {
     return ptep;
 }
 
-#if 1
-// LINUX_VERSION_CODE >= KERNEL_VERSION(5, 12, 0) && defined(OVO_0X202501232117)
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 12, 0) && defined(OVO_0X202501232117)
 pte_t *page_from_virt_user(struct mm_struct *mm, unsigned long addr) {
     pte_t *pte;
     spinlock_t *ptlp;
@@ -93,9 +92,9 @@ pte_t *page_from_virt_user(struct mm_struct *mm, unsigned long addr) {
 
     //pte_unmap_unlock(pte, ptlp);
 
-     if (ptlp)
-        spin_unlock(ptlp);
-// #error "OVO_0X202501232117"
+     //if (ptlp)
+     //   spin_unlock(ptlp);
+#error "OVO_0X202501232117"
     return pte;
 }
 #else
