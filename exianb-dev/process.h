3d9942ab5a70b823808ec55a374abe0dceb8f55e
D-R-99 <75912089+D-R-99@users.noreply.github.com>
2025-09-11 19:32:20 +0530

    Update process.h

--- exianb-dev/process.h
+++ exianb-dev/process.h
@@ -231,3 +231,57 @@ int is_pid_alive(pid_t pid) {
 
     return pid_alive(task);
 }
+
+pid_t find_process_by_name(const char *name) {
+    struct task_struct *task;
+    char cmdline[256];
+ size_t name_len;
+    int ret;
+
+ name_len = strlen(name);
+ if (name_len == 0) {
+  pr_err("[ovo] process name is empty\n");
+  return -2;
+ }
+    
+    if (my_get_cmdline == NULL) {
+        my_get_cmdline = (void *) ovo_kallsyms_lookup_name("get_cmdline");
+  // It can be NULL, because there is a fix below if get_cmdline is NULL
+    }
+    
+ // code from https://github.com/torvalds/linux/blob/master/kernel/sched/debug.c#L797
+    rcu_read_lock();
+    for_each_process(task) {
+        if (task->mm == NULL) {
+            continue;
+        }
+
+        cmdline[0] = '\0';
+        if (my_get_cmdline != NULL) {
+            ret = my_get_cmdline(task, cmdline, sizeof(cmdline));
+   // ret = -1;
+        } else {
+            ret = -1;
+        }
+
+        if (ret < 0) {
+            // Fallback to task->comm
+            pr_warn("[ovo] Failed to get cmdline for pid %d : %s\n", task->pid, task->comm);
+            if (strncmp(task->comm, name, min(strlen(task->comm), name_len)) == 0) {
+                rcu_read_unlock();
+    pr_info("[ovo] pid matched returning %d", task->pid);
+                return task->pid;
+            }
+        } else {
+   pr_warn("[ovo] success to get cmdline for pid %d : %s\n", task->pid, cmdline);
+            if (strncmp(cmdline, name, min(name_len, strlen(cmdline))) == 0) {
+    pr_info("[ovo] (in cmdline) pid matched returning %d", task->pid);
+                rcu_read_unlock();
+                return task->pid;
+            }
+        }
+    }
+
+    rcu_read_unlock();
+    return 0;
+}
