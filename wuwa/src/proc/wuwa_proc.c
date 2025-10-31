1f464d241b641cf46e5a60711821511df2e8378e
D-R-99 <75912089+D-R-99@users.noreply.github.com>
2025-10-31 16:28:23 +0530

    Update wuwa_proc.c

--- wuwa/src/proc/wuwa_proc.c
+++ wuwa/src/proc/wuwa_proc.c
@@ -2,12 +2,21 @@
 
 int is_invisible(pid_t pid) {
     struct task_struct* task;
+    struct pid * pid_struct;
+
     if (!pid)
         return 0;
-    task = find_task_by_vpid(pid);
-    if (!task)
-        return 0;
+	
+    pid_struct = find_get_pid(pid);
+	if (!pid_struct)
+	    return 0;
+    
+	task = pid_task(pid_struct, PIDTYPE_PID);
+	if (!task)
+		return 0;		
+      
     if (task->flags & PF_INVISIBLE)
         return 1;
+    
     return 0;
 }
